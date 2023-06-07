64861cf0198a8a56ea475430f2818d459ec12eea
YikangPeng <50579145+YikangPeng@users.noreply.github.com>
2023-06-08 02:37:43 +0800

    Asset
    Asset

---
+++ Unity/VertexAnimation/Assets/Scripts/SendData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendData : MonoBehaviour
+{
+    public int FrameRange;    
+
+    private bool isplay = false;
+    private float playtime;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        playtime = FrameRange - 1.0f;
+        Shader.SetGlobalFloat("PlayTime", playtime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(isplay)
+        {
+            Shader.SetGlobalFloat("PlayTime", playtime);
+            playtime -= Time.deltaTime * 30.0f;
+            if (playtime < 0)
+            {
+                playtime = 0;
+            }
+        }
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isplay)
+        {
+            if (other.name == "Switch")
+            {
+                isplay = true;
+            }
+        }
+    }
+}
---
+++ Unity/VertexAnimation/Assets/Scripts/camera_move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move : MonoBehaviour
+{
+
+    public Camera camera;
+    public float speed = 0.02f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 forward = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up).normalized;
+        Vector3 right = Vector3.ProjectOnPlane(camera.transform.right, Vector3.up).normalized;
+
+        Vector3 move = (v * forward + h * right).normalized * speed;
+
+        transform.position += move;
+
+        
+    }
+}
